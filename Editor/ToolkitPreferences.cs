a242b069aca68e283fd1e1b2d5960b0d4e4f7933
Simon Jackson <darkside@zenithmoon.com>
2022-05-11 17:45:23 +0100

    Revert moving Symbolic link functionality, too much trouble

--- Editor/ToolkitPreferences.cs
+++ Editor/ToolkitPreferences.cs
@@ -1,12 +1,7 @@
 // Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using RealityToolkit.Editor.Extensions;
 using RealityToolkit.Editor.Utilities;
-using RealityToolkit.Editor.Utilities.SymbolicLinks;
-using RealityToolkit.Extensions;
-using System.Linq;
-using UnityEditor;
 using UnityEngine;
 
 namespace RealityToolkit.Utilities.Editor
@@ -41,69 +36,5 @@ namespace RealityToolkit.Utilities.Editor
         }
 
         #endregion Debug Symbolic Links
-
-        #region Symbolic Link Preferences
-
-        private static bool isSymbolicLinkSettingsPathLoaded;
-        private static string symbolicLinkSettingsPath = string.Empty;
-
-        /// <summary>
-        /// The path to the symbolic link settings found for this project.
-        /// </summary>
-        public static string SymbolicLinkSettingsPath
-        {
-            get
-            {
-                if (!isSymbolicLinkSettingsPathLoaded)
-                {
-                    symbolicLinkSettingsPath = EditorPreferences.Get("_SymbolicLinkSettingsPath", string.Empty);
-                    isSymbolicLinkSettingsPathLoaded = true;
-                }
-
-                if (!EditorApplication.isUpdating &&
-                    string.IsNullOrEmpty(symbolicLinkSettingsPath))
-                {
-                    symbolicLinkSettingsPath = AssetDatabase
-                        .FindAssets($"t:{nameof(SymbolicLinkSettings)}")
-                        .Select(AssetDatabase.GUIDToAssetPath)
-                        .OrderBy(x => x)
-                        .FirstOrDefault();
-                }
-
-                return symbolicLinkSettingsPath;
-            }
-            set => EditorPreferences.Set("_SymbolicLinkSettingsPath", symbolicLinkSettingsPath = value);
-        }
-
-        private static bool isAutoLoadSymbolicLinksLoaded;
-        private static bool autoLoadSymbolicLinks = true;
-
-        /// <summary>
-        /// Should the project automatically load symbolic links?
-        /// </summary>
-        public static bool AutoLoadSymbolicLinks
-        {
-            get
-            {
-                if (!isAutoLoadSymbolicLinksLoaded)
-                {
-                    autoLoadSymbolicLinks = EditorPreferences.Get("_AutoLoadSymbolicLinks", true);
-                    isAutoLoadSymbolicLinksLoaded = true;
-                }
-
-                return autoLoadSymbolicLinks;
-            }
-            set
-            {
-                EditorPreferences.Set("_AutoLoadSymbolicLinks", autoLoadSymbolicLinks = value);
-
-                if (autoLoadSymbolicLinks && SymbolicLinker.Settings.IsNull())
-                {
-                    ScriptableObject.CreateInstance(nameof(SymbolicLinkSettings)).GetOrCreateAsset();
-                }
-            }
-        }
-
-        #endregion Symbolic Link Preferences
     }
 }
