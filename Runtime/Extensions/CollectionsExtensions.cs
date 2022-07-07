e0a4a3abecc64296ee2ecc0d8de32ff2fe149b1a
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2022-07-07 12:45:30 +0100

    Added a dictionary extension to ensure a dictionary item (#5)

--- Runtime/Extensions/CollectionsExtensions.cs
+++ Runtime/Extensions/CollectionsExtensions.cs
@@ -114,11 +114,11 @@ namespace RealityCollective.Extensions
         }
 
         /// <summary>
-        /// Exports the values of a uint indexed Dictionary as an Array
+        /// Exports the values of a uint indexed Dictionary as an Array.
         /// </summary>
-        /// <typeparam name="T">Type of data stored in the values of the Dictionary</typeparam>
-        /// <param name="input">Dictionary to be exported</param>
-        /// <returns>array in the type of data stored in the Dictionary</returns>
+        /// <typeparam name="T">Type of data stored in the values of the Dictionary.</typeparam>
+        /// <param name="input">Dictionary to be exported.</param>
+        /// <returns>array in the type of data stored in the Dictionary.</returns>
         public static T[] ExportDictionaryValuesAsArray<T>(this Dictionary<uint, T> input)
         {
             T[] output = new T[input.Count];
@@ -127,16 +127,42 @@ namespace RealityCollective.Extensions
         }
        
         /// <summary>
-        /// Validate if a list contains an item and add it if not found
+        /// Validate if a list contains an item and add it if not found.
         /// </summary>
-        /// <typeparam name="T">Data type used in the List</typeparam>
-        /// <param name="list">The instance of the List to validate</param>
-        /// <param name="item"></param>
+        /// <typeparam name="T">Data type used in the List.</typeparam>
+        /// <param name="list">The instance of the List to validate.</param>
+        /// <param name="item">The item of Type T to add to the list if not found</param>
         public static void EnsureListItem<T>(this IList<T> list, T item)
         {
             if (!list.Contains(item))
             {
                 list.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Validate if a <see cref="Dictionary{TKey, TValue}"/> contains an item and add it if not found."/>
+        /// </summary>
+        /// <remarks>
+        /// Will update the existing value when found unless overridden using the 'update' parameter
+        /// </remarks>
+        /// <typeparam name="TKey">Data type used in the Dictionary Key.</typeparam>
+        /// <typeparam name="TValue">Data type used in the Dictionary Value.</typeparam>
+        /// <param name="dictionary">The instance of the <see cref="Dictionary{TKey, TValue}"/> to validate.</param>
+        /// <param name="key">The Key of a <see cref="KeyValuePair{TKey, TValue}"/> to validate against the dictionary with.</param>
+        /// <param name="value">The Value of a <see cref="KeyValuePair{TKey, TValue}"/> to set the dictionary item with if required.</param>
+        /// <param name="update">By default, the Ensure function will override the existing dictionary value if found, if this is not required it can be overridden with this bool.  Setting this to <see cref="false"/> will leave the dictionary item untouched if found.</param>
+        public static void EnsureDictionaryItem<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value, bool update = true)
+        {
+            if(!dictionary.TryGetValue(key, out _))
+            {
+                dictionary.Add(key, value);
+                return;
+            }
+
+            if (update)
+            {
+                dictionary[key] = value;
             }
         }
     }
