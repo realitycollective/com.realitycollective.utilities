a721daa3b5a6fc3af3acac0773b6fa19ccb5c637
Simon Jackson <darkside@zenithmoon.com>
2022-07-11 23:27:53 +0100

    Updated Ensure Extensions to also return a boolean IF an item was added or actioned upon, where possible

--- Runtime/Extensions/CollectionsExtensions.cs
+++ Runtime/Extensions/CollectionsExtensions.cs
@@ -125,19 +125,23 @@ namespace RealityCollective.Extensions
             input.Values.CopyTo(output, 0);
             return output;
         }
-       
+
         /// <summary>
         /// Validate if a list contains an item and add it if not found.
         /// </summary>
         /// <typeparam name="T">Data type used in the List.</typeparam>
         /// <param name="list">The instance of the List to validate.</param>
         /// <param name="item">The item of Type T to add to the list if not found</param>
-        public static void EnsureListItem<T>(this IList<T> list, T item)
+        /// <returns>True if a new item was added to the collection</returns>
+        public static bool EnsureListItem<T>(this IList<T> list, T item)
         {
             if (!list.Contains(item))
             {
                 list.Add(item);
+                return true;
             }
+
+            return false;
         }
 
         /// <summary>
@@ -152,18 +156,20 @@ namespace RealityCollective.Extensions
         /// <param name="key">The Key of a <see cref="KeyValuePair{TKey, TValue}"/> to validate against the dictionary with.</param>
         /// <param name="value">The Value of a <see cref="KeyValuePair{TKey, TValue}"/> to set the dictionary item with if required.</param>
         /// <param name="update">By default, the Ensure function will override the existing dictionary value if found, if this is not required it can be overridden with this bool.  Setting this to <see cref="false"/> will leave the dictionary item untouched if found.</param>
-        public static void EnsureDictionaryItem<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value, bool update = true)
+        /// <returns>True if a new item was added to the collection</returns>
+        public static bool EnsureDictionaryItem<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value, bool update = true)
         {
             if(!dictionary.TryGetValue(key, out _))
             {
                 dictionary.Add(key, value);
-                return;
+                return true;
             }
 
             if (update)
             {
                 dictionary[key] = value;
             }
+            return false;
         }
     }
 }
